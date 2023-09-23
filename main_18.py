83832cafd95e32af3142350071b4508e1cc93223
Valentin-1981 <chibinyov@rambler.ru>
2023-09-23 23:47:22 +0300

    fifth commit

---
+++ main_18.py
@@ -0,0 +1,19 @@
+x = 0
+while x == 0:
+    try:
+        x = int(input('Введите число:'))
+        x += 5
+        print(x)
+    except ValueError:
+        print('Введите лучше число!')
+
+try:
+    x = 5 / 1
+except ZeroDivisionError:
+    print('Деление на ноль!')
+except ValueError:
+    print('Вы ввели что-то не так!')
+else:
+    print('else')
+finally:
+    print('Finally')
